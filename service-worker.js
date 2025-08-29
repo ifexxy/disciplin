ffef634d84e4c59eba1f04d720b8effc97fcfd2d
ifexxy <68091604+ifexxy@users.noreply.github.com>
2025-08-29 20:29:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Disciplin Service Worker
-// Version: 3.6.11 - Update this when making changes
+// Version: 3.6.12 - Update this when making changes
 
-const CACHE_NAME = 'disciplin-v3.6.11';
+const CACHE_NAME = 'disciplin-v3.6.12';
 const urlsToCache = [
     './',
     './index.html',
